6c9d15c86163066896d7112ca4358a741992d79e
Ignacio Sanhueza <ignacio.sanhueza.l@gmail.com>
2020-06-09 12:31:43 -0400

    Upload build for a test

---
+++ front/build/precache-manifest.7ad77e53ba78da556e511b3169a1020c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74be872fa2469bddf8e9764a5e1e609f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb2a28c17e2433bb780e",
+    "url": "/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "bbe1ba93fbd9315cf5d6",
+    "url": "/static/css/main.0da4e627.chunk.css"
+  },
+  {
+    "revision": "bb2a28c17e2433bb780e",
+    "url": "/static/js/2.e0196638.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e0196638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe1ba93fbd9315cf5d6",
+    "url": "/static/js/main.e4af2fdf.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "126ce8e04c3f17a4dfd25f1ddc615e19",
+    "url": "/static/media/circle-fill.126ce8e0.svg"
+  },
+  {
+    "revision": "77236959ca84b93f53af2aaf5e98e175",
+    "url": "/static/media/hand-thumbs-down.77236959.svg"
+  },
+  {
+    "revision": "07f69a97502bb3d7a2a3393dccca2059",
+    "url": "/static/media/hand-thumbs-up.07f69a97.svg"
+  },
+  {
+    "revision": "2813fc79155817c9105b72c991215b7e",
+    "url": "/static/media/heart-half.2813fc79.svg"
+  },
+  {
+    "revision": "452159056ecd699f6d17abdc57951a13",
+    "url": "/static/media/life-preserver.45215905.svg"
+  },
+  {
+    "revision": "02debf5f42cc11ebe276e1186c181394",
+    "url": "/static/media/person-bounding-box.02debf5f.svg"
+  },
+  {
+    "revision": "165196b3e0e48cf605ab1f5ed891b14a",
+    "url": "/static/media/plus-circle.165196b3.svg"
+  },
+  {
+    "revision": "e08562636198a48912b84f7bfc64002e",
+    "url": "/static/media/question-circle.e0856263.svg"
+  }
+]);
